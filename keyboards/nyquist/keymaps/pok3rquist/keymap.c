31f2d7a48b7cb2c97c6c4f9d2499ab197e4c3e53
= <=>
2018-10-14 21:10:07 +0200

    Move LEAD key to the left on pok3rquist

--- keyboards/nyquist/keymaps/pok3rquist/keymap.c
+++ keyboards/nyquist/keymaps/pok3rquist/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  | Alt  | LEAD |Raise |Space |Enter |Raise |Lower | Alt  |Macro | Ctrl |
+ * | Ctrl | LEAD | GUI  | Alt  |Raise |Space |Enter |Raise |Lower | Alt  |Macro | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_DEFAULT] = LAYOUT( \
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   MO(1),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LCTL, KC_LGUI, KC_LALT, KC_LEAD, MO(2),   KC_SPC,  KC_ENT,  MO(2),  MO(1),    KC_RALT, MO(3),   KC_RCTL \
+  KC_LCTL, KC_LEAD, KC_LGUI, KC_LALT, MO(2),   KC_SPC,  KC_ENT,  MO(2),  MO(1),    KC_RALT, MO(3),   KC_RCTL \
 ),
 
 /* Lower
